42686361de162c830ffa8486c32c2ca6751fac2f
Simmie <skajogbola@users.noreply.github.com>
2019-06-15 14:34:23 -0400

    article.js completed

---
+++ models/Article.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const ArticleSchema = new Schema ({
+	title: {
+		type: String,
+		unique: true
+	},
+	summary: {
+		type: String
+	},
+	link: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    // To save article
+	saved: {
+		type: Boolean,
+        default: false,
+        required:true
+    },
+    // To delete article
+    deleted: {
+        type: Boolean,
+        required: true,
+        default: false
+      },
+    // To take notes
+	note: [
+		{
+			type: Schema.Types.ObjectId,
+            ref: "Note",
+            required:false
+		}
+	]
+});
+
+const Article = mongoose.model("Article", ArticleSchema);
+
+module.exports = Article;
