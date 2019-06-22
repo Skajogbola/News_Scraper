7c76f1bea8ab66df99b7a4284224587002107de3
Simmie <skajogbola@users.noreply.github.com>
2019-06-22 14:22:11 -0400

    commit

--- routes/saved-routes.js
+++ routes/saved-routes.js
@@ -1,4 +1,4 @@
-const db = require("./models")
+const db = require("../models/index")
 
 module.exports = function (app) {
     app.get("/", function (req, res) {
--- routes/scrape-routes.js
+++ routes/scrape-routes.js
@@ -2,7 +2,7 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
 const request = require("request");
-const db = require("./models")
+const db = require("../models/index")
 
 //Routes
 module.exports = function (app) {
