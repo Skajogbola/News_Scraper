ecbfd5b787b128bd8ab3dadea39ec04ca41be26b
Simmie <skajogbola@users.noreply.github.com>
2019-06-22 14:05:09 -0400

    commit

--- server.js
+++ server.js
@@ -20,6 +20,11 @@ app.engine(
 );
 app.set("view engine", "handlebars");
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
 // Routes
 require("./routes/scrape-routes.js")(app);
 require("./routes/index-routes.js")(app);
@@ -41,6 +46,12 @@ mongoose.Promise = Promise;
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoScraper";
 mongoose.connect(MONGODB_URI);
 // mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+
+// // Database configuration with mongoose
+// mongoose.connect("mongodb://heroku_thdwtncq:v0db4jti38s60tnr86cs97bvvo@ds161483.mlab.com:61483/heroku_thdwtncq");
+// mongoose.connect("mongodb://heroku_wwsfpvv2:v0db4jti38s60tnr86cs97bvvo@ds035503.mlab.com:35503/:61483/heroku_wwsfpvv2");
+// mongodb://<dbuser>:<dbpassword>
+// var db = mongoose.connection;
 
 // Start the server
 app.listen(PORT, function () {
